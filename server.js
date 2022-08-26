54673311cdd7adec41f0d9370fde550f189c5990
Olanrewaju A. Olaboye <eyiwumi.olaboye@gmail.com>
2022-08-26 21:13:22 +0100

    get by id

--- server.js
+++ server.js
@@ -10,32 +10,67 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 const products = [
-  { id: 1, name: 'garri', quantity: 100, price: 300 },
-  { id: 2, name: 'beans', quantity: 300, price: 50 },
-  { id: 3, name: 'cassava', quantity: 1000, price: 300 },
-  { id: 4, name: 'corn', quantity: 500, price: 20 },
-  { id: 5, name: 'agbado', quantity: 80, price: 200 },
+  {
+    id: 1,
+    name: 'garri',
+    quantity: 100,
+    price: 300,
+    imageUrl:
+      'https://cdn.pixabay.com/photo/2022/08/19/01/06/ferris-wheel-7395944__480.jpg',
+  },
+  {
+    id: 2,
+    name: 'beans',
+    quantity: 300,
+    price: 50,
+    imageUrl:
+      'https://cdn.pixabay.com/photo/2022/01/28/09/26/azadi-tower-6974106__480.jpg',
+  },
+  {
+    id: 3,
+    name: 'cassava',
+    quantity: 1000,
+    price: 300,
+    imageUrl:
+      'https://cdn.pixabay.com/photo/2022/06/29/22/40/vervain-7292657__480.jpg',
+  },
+  {
+    id: 4,
+    name: 'corn',
+    quantity: 500,
+    price: 20,
+    imageUrl:
+      'https://cdn.pixabay.com/photo/2022/02/18/07/27/lake-7020123__480.jpg',
+  },
+  {
+    id: 5,
+    name: 'agbado',
+    quantity: 80,
+    price: 200,
+    imageUrl:
+      'https://cdn.pixabay.com/photo/2022/07/26/12/43/mountains-7345777__480.jpg',
+  },
 ];
 
 let users = [
   {
     id: 1,
     email: 'boyepanthera@gmail.com',
-    firsName: 'Olanrewaju',
+    firstName: 'Olanrewaju',
     lastName: 'Olaboye',
     password: '1234567',
   },
   {
     id: 2,
     email: 'jamal@gmail.com',
-    firsName: 'Olajide',
+    firstName: 'Olajide',
     lastName: 'Ibrahim',
     password: '1234567',
   },
   {
     id: 3,
     email: 'dayolonge@gmail.com',
-    firsName: 'Oladayo',
+    firstName: 'Oladayo',
     lastName: 'Longe',
     password: '1234567',
   },
@@ -43,7 +78,6 @@ let users = [
 
 //creating a controller/handleer for request to the base/index url : localhost:4000
 server.get('/', (req, res) => {
-  console.log(req);
   res.send('Hello World');
 });
 
@@ -51,8 +85,23 @@ server.get('/product', (req, res) => {
   return res.sendFile(path.join(__dirname, '/views/products.html'));
 });
 
+server.get('/product/:id', (req, res) => {
+  console.log(req.params);
+  res.sendFile(path.join(__dirname, 'views/single-product.html'));
+});
+
 server.get('/api/product', (req, res) => {
   return res.send({ products });
+});
+
+server.get('/api/product/:id', (req, res) => {
+  const productExist = products.find(
+    (product) => product.id === Number(req.params.id)
+  );
+  if (productExist) {
+    return res.status(200).send({ product: productExist });
+  }
+  return res.status(404).send({ message: 'product not found' });
 });
 
 server.get('/login', (req, res) => {
@@ -60,7 +109,7 @@ server.get('/login', (req, res) => {
 });
 
 server.post('/api/login', (req, res) => {
-  console.log('form data submitted', req.body);
+  console.log(req.body);
   const userExist = users.find((user) => user.email === req.body.email);
   if (userExist) {
     if (userExist.password === req.body.password) {
@@ -71,6 +120,11 @@ server.post('/api/login', (req, res) => {
   } else {
     return res.status(404).send("you don't have an account. signup instead");
   }
+});
+
+server.all('*', (req, res) => {
+  // res.status(404).send({ message: 'endpoint not found' });
+  return res.status(404).sendFile(path.join(__dirname, 'views/not-found.html'));
 });
 
 server.listen(port, () =>
